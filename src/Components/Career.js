804b6720c1ab8d52e5cb1690c9c51685382add3c
Aracturus <iconwebdev1@gmail.com>
2021-10-11 23:35:53 +0530

    form changed

--- src/Components/Career.js
+++ src/Components/Career.js
@@ -4,22 +4,26 @@ import './career.css'
 export default function Career() {
     return (
         <>
-       
-        <div className="full-container">
-            <h1 className="heading-career">Career</h1>
-        
+        <div className="career-box">
 
-        <div className="columns ">
-            <div className="column">
-            <p className="leftcontainer-p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis corrupti voluptatum, possimus neque ducimus, magni earum, nemo voluptatibus sint labore reprehenderit sit quasi molestias. Quidem facere praesentium asperiores maiores amet.
-                </p>
-            </div>
-            <div className="column">
-            <img className="image-container" src={'https://images.pexels.com/photos/34092/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500https://www.cognizant.com/us/en/strategic-cloud-partnerhttps://images.pexels.com/photos/34092/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcSet="" />
-            </div>
+       
+            <div className="container">
+                <h1 className="heading-career"><b>Career</b></h1>
             
+
+            <div className="columns ">
+                <div className="column container-para ">
+                <p className="container-para ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis corrupti voluptatum, possimus neque ducimus, magni earum, nemo voluptatibus sint labore reprehenderit sit quasi molestias. Quidem facere praesentium asperiores maiores amet.
+                    </p>
+                </div>
+                <div className="column">
+                <img className="image-container" src={'https://images.pexels.com/photos/34092/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500https://www.cognizant.com/us/en/strategic-cloud-partnerhttps://images.pexels.com/photos/34092/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcSet="" />
+                </div>
+                
+            </div>
+            </div>
         </div>
-        </div>
+
 
             
         </>
--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,88 +1,91 @@
 import React from 'react'
 import './forms.css'
+// import form from './form.jpeg'
+
 export default function Forms() {
     return (
         <>
    
-            <div className="forms-container">
+            <div className="career-box">
+            <div className="container">
+            
 
-                                <h1 className="form-heading">Reach Us</h1>
+            <div className="columns ">
+                <div className="column  ">
+                    <div className="formimage-container">
+                        <div >
+                            <img className="image-container1"src={'https://images.pexels.com/photos/4709285/pexels-photo-4709285.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcset="" />
 
-                        <div className="columns">
-                            <div className="column">
-                                <span className="heading-paragraph">
+                        </div> 
+                        <div >
+                        <img className="image-container2" src={'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcset="" />
 
-                                <p>
-                                    Fill up the form and our Team will get Back to you within 24 hours.
-                                </p>
-                                </span>
-
-                                <ol>
-                                <li>
-                                    <span className="li-btn">
-
-                                    <i className="fa fa-phone  reach-icon "></i>
-                                    <span>+0123456078910 </span>
-                                    </span>
-
-
-                                </li>
-                                <li>
-                                    <span className="li-btn">
-
-                                    <i className="fa fa-envelope  reach-icon "></i>
-                                    <span> hello@welcome.com</span>
-                                    </span>
-                                </li>
-                                <li> <span className="li-btn">
-
-                                    <i className="fas fa-map-marker-alt  reach-icon">        
-                                    </i>
-                                    <span>17th B main Road ,Motihari ,Bihar</span>
-                                </span>
-
-                                </li>
-                                </ol>
-                                <div className="social-icon">
-                                <span>
-                                    <i className="fab fa-facebook social-icons  "></i>
-
-                                </span>
-                                <span>
-                                    <i className="fab fa-twitter social-icons "></i>
-
-                                </span>
-                                <span>
-                                    <i className="fab fa-instagram social-icons "></i>
-
-                                </span>
-                            </div>
-                            </div>
-                            <div className="column">
-                            <div className="form-block column">
-                                <form action="">
-                                    <label htmlFor="name">Your Name</label><br/>
-                                    <span className="input-container">
-                                        <i className="fa fa-user icon-forms ">    </i>                                 
-                                        <input type="text" id="name" name="name" /><br/>
-                                    </span>
-
-                                    <label htmlFor="email">Mail</label><br />
-                                    <span className="input-container">
-                                        <span className="fa fa-envelope icon-forms"></span>
-                                        <input type="email" id="email" name="email" /><br/>
-                                    </span>
-
-
-                                    <label htmlFor="message">Message</label><br/>
-                                    <textarea name="message" id="message" cols="32" rows="10"></textarea><br/>
-                                    <input className="btn-sendmessage" type="submit" value="Send Message"/>
-                                </form> 
-
-                            </div>
-                            </div>
-                            
                         </div>
+                         <div >
+                         <img className="image-container3" src={'https://images.pexels.com/photos/52608/pexels-photo-52608.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcset="" />
+
+                        </div>
+                    </div>
+                
+                </div>
+                <div className="column form-container">
+                    <h2 className="form-heading"><b>Contact Us</b></h2>
+                    <p className="form-para">
+                        One of our workspace experts will reach out to you based on your communication preferences.
+                    </p>
+                    <div className="radio-box">
+                    <h3 className="radio-heading"><b>What option you're intrested in ?</b></h3>
+                        <div className=" radio-container">
+                            <label className=" ">
+                                <input type="radio" className="" name="rsvp"/>Office space
+                            </label>
+                            <label className=" ">
+                                <input type="radio" className="" name="rsvp"/>Membership
+                            </label>
+                            <label className=" " >
+                                <input type="radio" className="" name="rsvp"  />Dedicated Desk
+                            </label>
+                            <label className="" >
+                                <input type="radio" className="" name="rsvp"  />Other
+                            </label>
+                        </div>
+                    </div>
+
+                    <div  className="container">
+                     
+                         <h3 className="input-heading"><b>Your details</b></h3>
+                         <form action="">
+                             <span className='form-flex'>
+                                 <input type="text" placeholder="First Name*" name="" id="" />
+                                 <input type="text" placeholder="Last Name*" name="" id="" />
+                             </span> <br />
+                             <span className='form-flex'>
+                                 <input type="email" placeholder="Email*" name="" id="" />
+                                 <input type="number" placeholder="Phone Number" name="" id="" />
+                             </span> <br />
+                             <span className='form-flex'>
+                                 <input type="text" placeholder="Company Name*" name="" id="" />
+                                 <input type="number" placeholder="Company size" name="" id="" />
+                             </span><br />
+                             <span className='form-flex  '>
+                                 <input type="text" placeholder="Additional info" name="" className="add-info" />
+                                 
+                             </span><br />
+                             <p className='input-para'>
+                                 By clicking the button below you agree to our Terms of Service and have read and understood our Privacy Policy.
+
+                             </p>
+                             <button className="form-btn">Get in touch</button>
+                         </form>
+                         
+                     </div>
+                    
+
+
+                </div>
+                </div>
+                
+            </div>
             </div>
 
                                 
